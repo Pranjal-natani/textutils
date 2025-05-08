07c888dd1feb2c75985b2becededb312f3acfea6
Pranjal-natani <pranjalnatani1509@gmail.com>
2025-05-08 14:37:50 +0530

    Updates

---
+++ precache-manifest.9fa9d3f53cd02cd930efa35412c87706.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b61a0743ea9035f1f4438be5266c9f85",
+    "url": "/textutils/index.html"
+  },
+  {
+    "revision": "9882518c1ec0d4946084",
+    "url": "/textutils/static/css/main.f0cb387e.chunk.css"
+  },
+  {
+    "revision": "4309d6dc123f3d8f746d",
+    "url": "/textutils/static/js/2.6c811e04.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/textutils/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "9882518c1ec0d4946084",
+    "url": "/textutils/static/js/main.49a829e6.chunk.js"
+  },
+  {
+    "revision": "7a8be6afc65844edb6f3",
+    "url": "/textutils/static/js/runtime~main.0b59052a.js"
+  }
+]);
